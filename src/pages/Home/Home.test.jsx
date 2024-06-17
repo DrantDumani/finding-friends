14b9771afff63641c89cf96e614d0e86f49167ec
Darnell <darnjames@gmail.com>
2024-06-17 17:10:11 -0400

    Create home page and test

---
+++ src/pages/Home/Home.test.jsx
@@ -0,0 +1,50 @@
+import { Home } from "./Home";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+
+const mockData = [
+  {
+    _id: 1,
+    name: "Poppy",
+    thumbnail: "poppy.jpg",
+  },
+  {
+    _id: 2,
+    name: "Bros",
+    thumbnail: "bros.png",
+  },
+  {
+    _id: 3,
+    name: "Jr",
+    thumbnail: "jr.jpg",
+  },
+];
+const mockRoutes = [
+  {
+    path: "/",
+    element: <Home />,
+    loader: async () => mockData,
+  },
+];
+
+describe("Home page", () => {
+  it("Should render text saying How To Play", async () => {
+    const router = createMemoryRouter(mockRoutes, { initialEntries: ["/"] });
+    render(<RouterProvider router={router} />);
+    const title = await screen.findByRole("heading", { name: /How To Play/ });
+    expect(title).toBeInTheDocument();
+  });
+
+  it("should links, images, and captions for all games", async () => {
+    const router = createMemoryRouter(mockRoutes, { initialEntries: ["/"] });
+    render(<RouterProvider router={router} />);
+
+    const gameLinks = await screen.findAllByRole("link");
+
+    expect(gameLinks.length).toBe(3);
+    expect(gameLinks[0].textContent).toBe("Poppy");
+    expect(gameLinks[1].textContent).toBe("Bros");
+    expect(gameLinks[2].textContent).toBe("Jr");
+  });
+});
