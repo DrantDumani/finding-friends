61ab577b150a1f5740c8c24340385cd3ba7611e2
Darnell <darnjames@gmail.com>
2024-06-24 10:37:00 -0400

    Refactor convertMs to optionally include milliseconds

--- src/modules/convertTime.js
+++ src/modules/convertTime.js
@@ -1,12 +1,13 @@
-export function convertMs(timestamp) {
+export function convertMs(timestamp, includeMs = false) {
   const minutes = Math.floor(timestamp / 60000);
   const seconds = Math.floor((timestamp % 60000) / 1000);
   const ms = Math.floor(timestamp - minutes * 60000 - seconds * 1000);
 
-  return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
-    2,
-    "0"
-  )}:${String(Math.ceil(ms / 10) * 10)
+  const timeStr = `${String(minutes).padStart(2, "0")}:${String(
+    seconds
+  ).padStart(2, "0")}:${String(Math.ceil(ms / 10) * 10)
     .slice(0, 2)
     .padStart(2, "0")}`;
+
+  return includeMs ? timeStr : timeStr.slice(0, 5);
 }
